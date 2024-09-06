6ebdf1d71a94f0087c773632a0b8109717d7e680
Elizabeth <elizase405@gmail.com>
2024-09-06 01:52:40 -0700

    refactor

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,23 +3,10 @@
 from typing import List
 import re
 
-
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 seperator: str) -> str:
-    """
-    Function to returns the log message obfuscated
-    Args:
-        fields - a list of strings representing all fields to obfuscate
-        redaction - a string representing by what the field will be obfuscated
-        message - a string representing the log line
-        separator - a string representing by which character
-                    is separating all fields in the log line (message)
-    """
-
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns a log message obfuscated"""
     for f in fields:
-        message = re.sub(f'{f}=.*?{seperator}',
-                         f'{f}={redaction}{seperator}', message)
-
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
